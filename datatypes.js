7621e31b5f3ca9f0198014796102a3a6b79eb04c
Rajat Agrawal <rajatagrawal01@gmail.com>
2025-11-13 20:29:59 +0530

    Add files via upload
    var and datatypes

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+var a=34;
+var b="Hello";
+var c=true;
+
+var d=null;
+var e;
+
+
+console.log("Number: ",a);
+console.log("String: ",b);
+console.log("Boolean: ",c);
+console.log("Null: ",d);
+console.log("undefined: ",e);
+
+console.log(typeof a);
+
+
+
+
