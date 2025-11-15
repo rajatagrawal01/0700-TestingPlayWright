afa3948a68d14d8a897c733bb4bbbe88ee3733ba
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-15 15:13:15 +0530

    operators and conditions

---
+++ conditions.js
@@ -0,0 +1,30 @@
+// a=24;
+
+// if(a%2==0){
+//     console.log("Number is even");
+// }
+// else{
+//     console.log("Number is odd");
+    
+// }
+
+
+// var age=17.99999999;
+
+// if(age<18){
+//     console.log("Not eligible");
+// }
+// else if(age==18){
+//     console.log("Eligible for learnign");
+// }
+// else{
+//     console.log("Eligible");
+    
+// }
+
+
+var age=19;
+
+if(age>18){
+    console.log("eligible");
+}
---
+++ operators.js
@@ -0,0 +1,47 @@
+
+                    // Arithmetic Operators
+
+// console.log("Addition",24+2);
+// console.log("Sub",24-2);
+// console.log("multiplication",24*2);
+// console.log("Divison",24/2);
+// console.log("Remainder",19%5);
+
+                    // Comparison Operators
+
+// >,<,>=,<=,==,===,!==,!=
+
+// console.log(23<2);
+// console.log(23>2);
+// console.log(23<=2);
+// console.log(23>=23);
+// console.log(23==23);
+// console.log(23!=23);
+// console.log(23==="23");
+// console.log(23!=="23");
+
+                    // Assignment operatos
+
+// var a=25;
+// var b=30;
+
+// a=b
+// a+=2
+// console.log(a);
+
+
+                    // Step Operators
+// var a=25
+// a++
+// console.log(a);
+ 
+
+                    // logical operator
+var a=17
+
+// console.log(a%5==0 && a%3==0);
+
+// console.log(a%5==0 || a%3==0);
+
+
+
