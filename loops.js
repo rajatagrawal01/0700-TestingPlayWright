eaefbf0ac5fafa11b87309d0591b964b3cecdbeb
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-20 20:13:12 +0530

    looops

---
+++ loops.js
@@ -0,0 +1,42 @@
+// =============================Do-while loop==========================
+// var i=100;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+// =============================While loop==========================
+
+// var i=1;
+
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// =============================For loop==========================
+// Increment:-
+// for(var i=1;i<=10;i++){
+//     console.log(i);
+// }
+// Decrement
+// for(var i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// for(var i=1;i<=10;i++){
+//     if(i%2==0){
+//         console.log(i,": is Even");
+//     }
+//     else{
+//         console.log(i,": is Odd");
+
+//     }
+// }
+
+for(let i=1;i<=100;i++){
+    if(i%3==0 && i%5==0){
+        console.log(i);
+    }
+}
