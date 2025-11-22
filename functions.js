93fc45526e735d768fa87db98241adeb01a3b6d5
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-22 15:14:05 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,40 @@
+// function evenOddCheck(a) {
+//   if (a % 2 == 0) {
+//     console.log(a, ":Number is even");
+//   } else {
+//     console.log(a, ":Number is odd");
+//   }
+// }
+
+// function eligibility(age) {
+//   if (age < 18) {
+//     console.log("Not eligible");
+//   } else if (age == 18) {
+//     console.log("Eligible for learnign");
+//   } else {
+//     console.log("Eligible");
+//   }
+// }
+
+// evenOddCheck(25);
+// eligibility(25);
+
+function hello(){
+    console.log("Hello, how are you?");
+}
+
+function maths(a,b){
+    console.log("Sum of ",a," and ",b," is : ",a+b);
+    console.log("Sub of ",a," and ",b," is : ",a-b);
+    console.log("Mul of ",a," and ",b," is : ",a*b);
+    console.log("Div of ",a," and ",b," is : ",a/b);
+    console.log(" ");
+}
+
+hello();
+maths(34,34)
+maths(45,14);
+maths(134,234);
+maths(14,47);
+maths(134,12412);
+
