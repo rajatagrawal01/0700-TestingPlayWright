1ed36c708128409fc2deaca6a599c2060a4b9eb2
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-25 20:16:50 +0530

    array

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var x=[23,22,233,234,355,5746,"Hello",true];
+// length=8
+// last index=7
+
+
+//console.log(x); //Print complete array
+//console.log(arrayname[indexOfElement]); // Print one element
+// console.log(x[4]);
+console.log(x[3]);
+
+
+// for(let i=0;i<=10;i++){
+//     console.log(x[i]);
+// }
+
+
+for(let i=0;i<x.length;i++){
+    console.log(x[i]);
+}
