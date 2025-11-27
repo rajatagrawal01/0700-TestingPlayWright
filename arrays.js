6c85938bb332d3d3f4eb9c5b757e1615bd6ddbf1
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-11-27 19:54:11 +0530

    arr methods

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var x=[23,22,233,234,355,5746,"Hello",true];
+// var x=[23,22,233,234,355,5746,"Hello",true];
 // length=8
 // last index=7
 
@@ -6,7 +6,7 @@ var x=[23,22,233,234,355,5746,"Hello",true];
 //console.log(x); //Print complete array
 //console.log(arrayname[indexOfElement]); // Print one element
 // console.log(x[4]);
-console.log(x[3]);
+// console.log(x[3]);
 
 
 // for(let i=0;i<=10;i++){
@@ -14,6 +14,69 @@ console.log(x[3]);
 // }
 
 
-for(let i=0;i<x.length;i++){
-    console.log(x[i]);
-}
+// for(let i=0;i<x.length;i++){
+//     console.log(x[i]);
+// }
+
+
+
+var arr=[23,22,233,234,355,5746,"Hello",true];
+
+
+
+
+
+// pop()- removes last value    shift()- removes first value
+// push(value)- adds last value    unshift(value)- adds first value
+
+// ===========================Array Unshift===========================
+// console.log("Before Shift : ",arr);
+// arr.unshift("Dalchand");
+// console.log("After Shift : ",arr);
+
+// ===========================Array Shift===========================
+// console.log("Before Shift : ",arr);
+// var first =arr.shift()
+// console.log("After Shift : ",arr);
+// console.log("Shifted value : ",first);
+
+
+
+
+// ===========================Array Pop===========================
+// console.log("Before Pop : ",arr);
+// var last=arr.pop()
+// console.log("After Pop : ",arr);
+// console.log("Popped value : ",last);
+
+
+// ===========================Array Push===========================
+// console.log("Before Push : ",arr);
+// arr.push(23);
+// console.log("After Push : ",arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arr.push(6); Adding value in last
+// arr.unshift(0); Adding value from first
+// arr.shift(); Removing value from first
+// var arr3 = arr.concat([1,2,3,4,5])
+// var arr4 = arr2.concat(['d','e','f']);
+// arr2.join() Defines how array will print
+// arr.indexOf(3) Provides index of an element
+// arr.includes(3) Provides availability of a value (return boolen)
+// arr.reverse() Prints reverse of an array
+// arr.every()
+// var double =arr.map(x=> x*2);
+// console.log(double);
+// arr.every(x=>x>0)
