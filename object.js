3ee191ebd57ce2f06525a8e9d3a0c08f06c0ea4d
rajatagrawal01 <rajatagrawal01@gmail.com>
2025-12-04 20:18:44 +0530

    object

---
+++ object.js
@@ -0,0 +1,51 @@
+// var a=[23,34,55,67,98]
+
+// var a={
+//     maths:23,
+//     eng:34,
+//     phy:55,
+//     chem:67,
+//     hin:98
+// }
+// console.log(a);
+
+// var a=[234,234,2342,45,58]
+var stu={
+    name:"Rajat",
+    age:30,
+    address:"Indore",
+    number:783243784,
+    email:'kfa@qlsfh.com',
+    marks:[23,55,77,45,98]
+}
+
+stu.height="100cm"
+stu.name="Dalchand"
+
+console.log(stu.marks[2]);
+
+
+// var stu = [
+//   {
+//     stu_name: "Rajat",
+//     age: 30,
+//   },
+//   {
+//     stu_name: "Dalchand",
+//     age: 25,
+//   },
+//   {
+//     stu_name: "kanchan",
+//     age: 25,
+//   },
+//   {
+//     stu_name: "chetan",
+//     age: 25,
+//   },
+//   {
+//     stu_name: "abhishek",
+//     age: 25,
+//   },
+// ];
+
+// console.log(stu[4].stu_name);
